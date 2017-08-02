f52b4975de87ce5c3b5a3280640f5e2ebf17cc20
MarcinKałat <mkalat@mkalat.pl>
2017-08-03 00:54:15 +0200

    added login routes

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
-<!DOCTYPE html>
+<?php
+require 'core.php';
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+if (isset($_GET['action']) && $_GET['action'] == 'login')
+{
+?>
+    <!DOCTYPE html>
+    <html>
+            <head>
+                <meta charset="UTF-8">
+                <title></title>
+            </head>
+        <body>
+            <?php
+            // put your code here
+            ?>
+        </body>
+    </html>
+<?php
+}
+else if (isset($_GET['action']) && $_GET['action'] == 'logout') 
+{
+    logout();
+    
+   
+}
+    
+    
+    
+    
+    
+
+
+
+
+?>
+
+
